9b686848bf704503a36aad8c83d728ed4478c8a7
payalkaur <payalkaur1706@gmail.com>
2022-07-31 10:36:05 +0530

    Pins done

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
 
 const App = () => {
 return(
-    <Routes>
-        <Route path="login" element={<Login />}/>
-        <Route path="/*" element={<Home />}/>
-    
-    </Routes>
-)
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
+        <Routes>
+            <Route path='login' element={<Login />}/>
+            <Route path='/*' element={<Home />}/>
+        </Routes>
+    </GoogleOAuthProvider>
+    )
 }
 
 export default App
